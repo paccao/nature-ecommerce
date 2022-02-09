c3108039716953d8a515b689f8840efa65a89e0b
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-02-09 16:18:58 +0100

    base styling in index and app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,38 @@
 import styled from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import HomePage from './pages/HomePage'
+import { lightTheme } from './themes/appTheme'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-	return <PageWrapper className="App">Insert content here. 🐳</PageWrapper>
+	return (
+		<ThemeProvider theme={lightTheme}>
+			<PageWrapper className="App">
+				<header>Nature e-commerce</header>
+
+				<main>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="*" element={<div>404 page</div>} />
+					</Routes>
+				</main>
+			</PageWrapper>
+		</ThemeProvider>
+	)
 }
 
 const PageWrapper = styled.div`
-	display: flex;
-	justify-content: center;
+	background-color: ${(props) => props.theme.bgColor};
+	max-width: 980px;
+	margin: 0 auto;
+
+	min-height: 100vh;
+
+	main {
+		padding: 1rem;
+		width: 100%;
+		background-color: ${(props) => props.theme.bgColor};
+	}
 `
 
 export default App
---
+++ client/src/themes/appTheme.ts
@@ -0,0 +1,13 @@
+export const lightTheme = {
+	bgColor: '#E4E4E4',
+	__bgColorLargePageWidth: '#ffffff',
+	textColor: '#161409',
+
+	textHighEmph: '87%',
+	textMediumEmph: '60%',
+	textLowEmpEmph: '38%',
+
+	borderRadius: '15px',
+
+	hr: '1px solid white;',
+}
