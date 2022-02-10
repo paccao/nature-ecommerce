e8d0b902540a3e99b4dcbce637bc669c6f0cdf7a
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-02-10 09:43:36 +0100

    set up query client, installed mui

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
 	</React.StrictMode>,
 	document.getElementById('root'),
 )
