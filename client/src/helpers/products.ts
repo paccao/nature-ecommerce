7e22e6d179ef51762c3481ab909bce27f2d6abdc
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-02-10 11:27:49 +0100

    helper for fetching specific product

--- client/src/helpers/products.ts
+++ client/src/helpers/products.ts
@@ -1,4 +1,7 @@
 import { Product } from '../models/Product'
 
-export const getProducts = async (): Promise<Product> =>
+export const fetchProducts = async (): Promise<Product> =>
 	await (await fetch('http://localhost:8080/api/products')).json()
+
+export const fetchSpecificProduct = async (id: string): Promise<Product> =>
+	await (await fetch(`http://localhost:8080/api/products/${id}`)).json()
