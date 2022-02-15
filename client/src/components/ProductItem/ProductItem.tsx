9b77beb981df213606704577c9be7cfed8af449a
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-02-15 13:26:05 +0100

    Styled add to cart buttons and input fields

--- client/src/components/ProductItem/ProductItem.tsx
+++ client/src/components/ProductItem/ProductItem.tsx
@@ -24,7 +24,29 @@ export default function ProductItem({ product }: Props) {
 				<p>{product.description}</p>
 				<div className="bottom">
 					<b>{`${product.price}kr`}</b>
-					<GenericButton innerText="Details" />
+					<div className="input-group">
+						<input
+							type="button"
+							value="-"
+							className="button-minus cart-button"
+							data-field="quantity"
+						/>
+						<input
+							type="number"
+							step="1"
+							max="99"
+							value="1"
+							name="quantity"
+							className="quantity-field"
+						/>
+						<input
+							type="button"
+							value="+"
+							className="button-plus cart-button"
+							data-field="quantity"
+						/>
+					</div>
+					<GenericButton innerText="Buy" />
 				</div>
 			</Information>
 		</ProductCard>
@@ -49,9 +71,65 @@ const Information = styled.article`
 	}
 	.bottom {
 		display: flex;
-		justify-content: space-around;
 		align-items: baseline;
+		gap: 0.3rem;
 		padding: 0.5rem 0rem;
+		margin: 0rem 0.2rem;
+
+		b {
+			font-size: 0.9rem;
+		}
+	}
+
+	.input-group input {
+		all: unset;
+	}
+	.input-group input[type='number'] {
+		-webkit-appearance: textfield !important;
+		-moz-appearance: textfield !important;
+		appearance: textfield !important;
+	}
+
+	.input-group input[type='number']::-webkit-inner-spin-button,
+	.input-group input[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+	}
+	.input-group {
+		margin-left: auto;
+		display: flex;
+		max-width: 4rem;
+		justify-content: space-around;
+		border-radius: ${(props) => props.theme.borderRadius};
+		border: 1px solid #dcd3d3;
+		/* padding: 0rem 0.2rem; */
+		line-height: 100%;
+
+		.quantity-field {
+			width: 60%;
+			text-align: center;
+			transition: all 0.2s;
+		}
+		.quantity-field:hover {
+			color: ${(props) => props.theme.accentColor};
+		}
+		.cart-button {
+			width: 20%;
+			padding: 0.2rem 0.35rem;
+			text-align: center;
+			cursor: pointer;
+			transition: all 0.2s;
+		}
+		.button-minus {
+			border-top-left-radius: ${(props) => props.theme.borderRadius};
+			border-bottom-left-radius: ${(props) => props.theme.borderRadius};
+		}
+		.button-plus {
+			border-top-right-radius: ${(props) => props.theme.borderRadius};
+			border-bottom-right-radius: ${(props) => props.theme.borderRadius};
+		}
+		.cart-button:hover {
+			background-color: #f0f0f0;
+		}
 	}
 `
 
--- client/src/components/global/GenericButton.tsx
+++ client/src/components/global/GenericButton.tsx
@@ -14,8 +14,14 @@ export default GenericButton
 const Button = styled.button`
 	all: unset;
 	cursor: pointer;
-	padding: 0.3rem 0.8rem;
+	padding: 0.3rem 0.7rem;
+	font-size: 0.85em;
 	background-color: ${(props) => props.theme.textColor};
 	color: ${(props) => props.theme.__bgColorLargePageWidth};
 	border-radius: ${(props) => props.theme.borderRadius};
+	transition: all 0.2s;
+
+	:hover {
+		color: ${(props) => props.theme.accentColor};
+	}
 `
