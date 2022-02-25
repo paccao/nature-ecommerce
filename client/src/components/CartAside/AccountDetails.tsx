0cf9a357906db41bee2e731986b130f2142e240e
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-02-25 15:38:12 +0100

    mvp of rendereing account details

--- client/src/components/CartAside/AccountDetails.tsx
+++ client/src/components/CartAside/AccountDetails.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import useAccountDetails from '../../hooks/useAccountDetails'
 
 type Props = {}
 
 function AccountDetails({}: Props) {
-	return <div>AccountDetails</div>
+	const { data } = useAccountDetails()
+	return (
+		<article>
+			<h2>{data?.account.name}</h2>
+			<p>{data?.account.adress}</p>
+		</article>
+	)
 }
 
 export default AccountDetails
