7d61cf0622780ce5f9d7025d90716597f9bce8d5
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-03-21 12:22:55 +0100

    Initial html for login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
+import styled from 'styled-components'
+import GenericButton from '../components/global/GenericButton'
 
 type Props = {}
 
 function Login({}: Props) {
-	return <div>Login</div>
+	function submitHandler(e: SyntheticEvent) {
+		e.preventDefault()
+	}
+	return (
+		<Wrapper>
+			<section className="bottom">
+				<h1>Welcome back</h1>
+				<form onSubmit={submitHandler}>
+					<input type="email" name="email" />
+					<input type="password" name="password" />
+					<GenericButton {...{ innerText: 'Log in', type: 'submit' }} />
+				</form>
+			</section>
+		</Wrapper>
+	)
 }
 
 export default Login
+
+const Wrapper = styled.div``
