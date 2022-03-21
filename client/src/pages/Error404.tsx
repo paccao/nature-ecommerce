c36343004fb6eba57ace6235a967b4da460c1e48
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-03-21 12:00:22 +0100

    updated 404 page

--- client/src/pages/Error404.tsx
+++ client/src/pages/Error404.tsx
@@ -4,18 +4,23 @@ import styled from 'styled-components'
 export default function Error404() {
 	return (
 		<Page404Container>
-			<h1> 404</h1>
-			<h2> Page Not Found</h2>
+			<h1> Oooops. 😲</h1>
+			<h2> This page was not found.</h2>
 			<a href="/">Home page</a>
 		</Page404Container>
 	)
 }
 
 const Page404Container = styled.div`
-	height: 100vh;
+	height: 85vh;
 	border-radius: ${(props) => props.theme.borderRadius};
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
+	text-align: center;
+
+	a {
+		color: ${(props) => props.theme.accentColor};
+	}
 `
