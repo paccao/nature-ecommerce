81ff79ae7fc1bf6f353551b054e82c0fd77a8213
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-03-21 12:41:59 +0100

    Initial styling for login form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -13,8 +13,8 @@ function Login({}: Props) {
 			<section className="bottom">
 				<h1>Welcome back</h1>
 				<form onSubmit={submitHandler}>
-					<input type="email" name="email" />
-					<input type="password" name="password" />
+					<input type="email" name="email" placeholder="email" />
+					<input type="password" name="password" placeholder="password" />
 					<GenericButton {...{ innerText: 'Log in', type: 'submit' }} />
 				</form>
 			</section>
@@ -24,4 +24,43 @@ function Login({}: Props) {
 
 export default Login
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+	form {
+		max-width: 500px;
+		max-height: 250px;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		input {
+			all: unset;
+			border-bottom: 2px solid #888;
+			padding: 0.1rem 0.1rem 0.3rem 0.1rem;
+		}
+
+		input:hover,
+		input:active,
+		input:focus {
+			border-bottom: 2px solid ${(props) => props.theme.accentColor};
+			color: ${(props) => props.theme.accentColor};
+
+			::placeholder {
+				color: ${(props) => props.theme.accentColor};
+			}
+		}
+
+		button {
+			margin-top: 1rem;
+			border-radius: 7px;
+			background-color: ${(props) => props.theme.accentColor};
+			font-size: 1em;
+			font-weight: 500;
+			text-align: center;
+		}
+
+		button:hover,
+		button:active,
+		button:focus {
+			color: ${(props) => props.theme.textColor};
+		}
+	}
+`
