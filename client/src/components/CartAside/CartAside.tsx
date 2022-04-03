c95110787c76b528aa97bfe09b124886e189ba76
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-04-03 19:09:45 +0200

    made cartaside not expand to infinity if the products wrap

--- client/src/components/CartAside/CartAside.tsx
+++ client/src/components/CartAside/CartAside.tsx
@@ -42,7 +42,7 @@ export default CartAside
 
 const AsideWrapper = styled.aside`
 	background-color: #fff;
-	min-height: 87vh;
+	height: 87vh;
 	border-radius: ${(props) => props.theme.borderRadius};
 	border-bottom-right-radius: ${(props) => props.theme.borderRadius};
 	border-top-right-radius: ${(props) => props.theme.borderRadius};
