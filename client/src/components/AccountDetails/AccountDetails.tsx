1a70b2a502af10ceee5f43b2d3a8f352f3dd5c11
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-04-04 20:20:49 +0200

    Added login button and dynamic text based on log in status

--- client/src/components/AccountDetails/AccountDetails.tsx
+++ client/src/components/AccountDetails/AccountDetails.tsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import temporaryUser from '../../helpers/temporaryUser'
-import useAccountDetails from '../../hooks/useAccountDetails'
+import isLoggedIn from '../../atoms/loggedInState'
+import { useRecoilState } from 'recoil'
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
 
-type Props = {}
+function AccountDetails() {
+	const [loggedInState, _] = useRecoilState(isLoggedIn)
+	const navigate = useNavigate()
+	const submitHandler = () => {
+		navigate('/login')
+	}
 
-function AccountDetails({}: Props) {
-	const { data } = useAccountDetails()
-	console.log(data)
 	return (
-		<article>
-			{data ? (
-				<>
-					<h2>{data.account?.name}</h2>
-					<p>{data.account?.adress}</p>
-				</>
-			) : null}
-		</article>
+		<Wrapper>
+			<span>Welcome friend!</span>
+			{loggedInState ? (
+				<article>Welcome customer!</article>
+			) : (
+				<button onClick={submitHandler}>Log in</button>
+			)}
+		</Wrapper>
 	)
 }
 
 export default AccountDetails
+
+const Wrapper = styled.div`
+	margin: 0.5rem 0.2rem;
+	display: flex;
+	align-items: baseline;
+
+	button {
+		all: unset;
+
+		margin-left: auto;
+		cursor: pointer;
+		padding: 0.3rem 0.7rem;
+		font-size: 0.85em;
+		background-color: ${(props) => props.theme.textColor};
+		color: ${(props) => props.theme.__bgColorLargePageWidth};
+		border-radius: ${(props) => props.theme.borderRadius};
+		transition: all 0.2s;
+
+		:hover {
+			color: ${(props) => props.theme.accentColor};
+		}
+	}
+`
