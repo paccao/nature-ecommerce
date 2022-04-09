a8695d5a8bbde8f0594ce4b3f7fc95b0dfccfbe8
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-04-09 15:58:05 +0200

    fixed api domain to fetch in prod

--- client/src/helpers/apiDomain.ts
+++ client/src/helpers/apiDomain.ts
@@ -1,4 +1,4 @@
 export default function apiDomain(): string {
 	const production = process.env.NODE_ENV === 'production'
-	return production ? '' : 'http://localhost:8080'
+	return production ? window.origin : 'http://localhost:8080'
 }
