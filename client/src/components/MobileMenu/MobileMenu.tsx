8c2185c5bf43e364a5f8f905e020854d73968418
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-05-07 08:34:37 +0200

    Set up initial states for toggling menu

--- client/src/components/MobileMenu/MobileMenu.tsx
+++ client/src/components/MobileMenu/MobileMenu.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import menuOpenState from '../../atoms/menuOpenState'
+import menuActiveClassname from '../../atoms/menuActiveClassname'
+import { useRecoilState } from 'recoil'
 type Props = {}
 
 const MobileMenu: React.FC<Props> = (props) => {
-	return <MenuButton>📃</MenuButton>
+	const [isOpen, setIsOpen] = useRecoilState(menuOpenState)
+	const [activeClassname, setActiveClassname] =
+		useRecoilState(menuActiveClassname)
+
+	const toggleMenu = (): void => {
+		setIsOpen(() => !isOpen)
+		setActiveClassname(() => (isOpen ? '' : 'active'))
+	}
+
+	return (
+		<MenuButton className={`${activeClassname}`} onClick={toggleMenu}>
+			<span>{isOpen ? '✖' : '📃'}</span>
+		</MenuButton>
+	)
 }
 
 const MenuButton = styled.button`
 	all: unset;
 	font-size: 1.3em;
+	cursor: pointer;
 `
 
 export default MobileMenu
