167ac5f25c70253b003d338b0843614aae8ba764
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-05-30 07:57:00 +0200

    before testing older dockerfile versions

--- client/src/helpers/apiDomain.ts
+++ client/src/helpers/apiDomain.ts
@@ -1,4 +1,4 @@
 export default function apiDomain(): string {
-	const production = process.env.NODE_ENV === 'production'
-	return production ? window.origin : 'http://localhost:8080'
+	const isProduction = process.env.NODE_ENV === 'production'
+	return isProduction ? window.origin : 'http://localhost:8080'
 }
